cd32eae1cd7e0645515e99c5a767e0d7805e36c3
Jakub Kujawa <ZK_Jakub@proton.me>
2024-02-08 23:24:02 +0100

    fix(script): create directory to store rpms

--- config/scripts/printer-drivers.sh
+++ config/scripts/printer-drivers.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 
 RPM_ROOT="/tmp/rpms"
 
+mkdir -p "$RPM_ROOT"
+
 wget -O "${RPM_ROOT}/brother-hl-1112E-CUPS.rpm" https://support.brother.com/g/b/files/dlf/dlf007073/hl1110cupswrapper-3.0.1-1.i386.rpm
 wget -O "${RPM_ROOT}/brother-hl-1112E-LPR.rpm" https://support.brother.com/g/b/files/dlf/dlf007071/hl1110lpr-3.0.1-1.i386.rpm
 
