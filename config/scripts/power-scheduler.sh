3dc5a6116f4076d543a9220ae8189bb43704b412
Jakub Zenon Kujawa <ZK_Jakub@proton.me>
2024-04-20 17:57:00 +0200

    refactor(script): replace the usage of `wget` with `curl`

--- config/scripts/power-scheduler.sh
+++ config/scripts/power-scheduler.sh
@@ -3,11 +3,9 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-wget "https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo" \
-    -O /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
+curl -o /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo "https://copr.fedorainfracloud.org/coprs/kylegospo/system76-scheduler/repo/fedora-$(rpm -E %fedora)/kylegospo-system76-scheduler-fedora-$(rpm -E %fedora).repo"
 
-wget "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo?arch=x86_64" \
-    -O /etc/yum.repos.d/_copr_ublue-os-staging.repo
+curl -o /etc/yum.repos.d/_copr_ublue-os-staging.repo "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo?arch=x86_64"
 
 if rpm -qa | grep power-profiles-daemon; then
     rpm-ostree override remove power-profiles-daemon gamemode --install=system76-scheduler --install=gnome-shell-extension-system76-scheduler \
@@ -21,7 +19,7 @@ systemctl enable com.system76.Scheduler.service
 systemctl enable tuned.service
 
 mkdir -p /usr/etc/system76-scheduler
-wget https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl -O /usr/etc/system76-scheduler/config.kdl
+curl -o /usr/etc/system76-scheduler/config.kdl https://raw.githubusercontent.com/ublue-os/bazzite/main/system_files/desktop/shared/usr/etc/system76-scheduler/config.kdl
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo_system76_scheduler_fedora.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os-staging.repo
